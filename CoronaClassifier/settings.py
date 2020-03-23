f8d6801b50e34d20c3f7d91522b538814e41bf98
adityap27 <adityapurohit27@gmail.com>
2020-03-23 22:56:32 +0530

    heroku deployment changes

--- CoronaClassifier/settings.py
+++ CoronaClassifier/settings.py
@@ -125,7 +125,7 @@ STATICFILES_DIRS=[
 ]
 STATIC_ROOT = os.path.join(BASE_DIR,'assets')
 #  Add configuration for static files storage using whitenoise
-STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 MEDIA_ROOT  = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/ML model/'
